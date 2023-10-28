30efb51f58514f027ea59d0c51c716bebcf018a3
Fletch <fletchmakesstuff@gmail.com>
2023-10-28 12:27:31 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+	channel: "fletchmakes",		//set your channel name between the quotes, ie: "eateren"
+	waitTimeUser: 10,		//set the cooldown time per user in seconds
+	waitTimeGlobal: 0,		//set the global cooldown time in seconds
+	command: "confetti",	//the command to make it rain
+};
